893fce7d5f1e834f27c41dd5dcb4e3583500f668
kimhaun <wha02068@gmail.com>
2020-11-14 12:35:30 +0900

    onResume 메인 화면  - 홈 화면/다른 액티비티에서 돌아올 때마다 실행됨.

--- app/src/main/java/com/appisna/blogspot/lifecycle/MainActivity.kt
+++ app/src/main/java/com/appisna/blogspot/lifecycle/MainActivity.kt
@@ -16,6 +16,21 @@ class MainActivity : BaseActivity() {
         Log.d("메인화면", "onCreate 실행")
 
     }
+
+    //이어가다
+    override fun onResume() {
+        //onCreate -> onResume순서
+        //처음 만들어질 때, 뒤로가기, 홈버튼갔다가 돌아오는 상황도 포함
+        super.onResume()
+        Log.d("메인 화면", "onResume 실행")
+    }
+
+    //액티비티가 화면에서 사라지려고 할 때 실행되는 함수. onResume의 반대 개념.
+    //finish로 종료 + 다른화면에 의해 가려짐, 홈 등으로 빠져나갈 때
+    override fun onPause() {
+        super.onPause()
+
+    }
     override fun setupEvents() {
 
         moveBtn.setOnClickListener {
