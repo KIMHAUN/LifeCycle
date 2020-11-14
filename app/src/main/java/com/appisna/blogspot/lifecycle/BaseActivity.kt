7fcd8590356edd7a8bbcc0c569d55c4a0f907150
kimhaun <wha02068@gmail.com>
2020-11-14 12:07:58 +0900

    Initial commit

---
+++ app/src/main/java/com/appisna/blogspot/lifecycle/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.appisna.blogspot.lifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
