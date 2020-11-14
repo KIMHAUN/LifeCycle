a04475b74513742f61e461b4324fbb2a76d126e5
kimhaun <wha02068@gmail.com>
2020-11-14 12:10:48 +0900

    메인 화면 onCreate 로그 확인

--- app/src/main/java/com/appisna/blogspot/lifecycle/MainActivity.kt
+++ app/src/main/java/com/appisna/blogspot/lifecycle/MainActivity.kt
@@ -2,6 +2,7 @@ package com.appisna.blogspot.lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +10,8 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
+
+        Log.d("메인화면", "onCreate 실행")
 
     }
     override fun setupEvents() {
